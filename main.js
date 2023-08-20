b5407045617c9fa38897d19849b212a13f316ac9
Katana <user@katana.com>
2023-08-20 18:15:58 +0530

    Print Js files of directory

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-console.log("Hello World");
-
-console.log("Trying to Move My JS Files Here");
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory 
+//const directoryPath = path.join(__dirname, 'Documents');
+//passsing directoryPath and callback function
+//fs.readdir(directoryPath, function (err, files) {
+fs.readdir('/home/megh/Documents', function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+		// find JS files and move them to Documents/Js
+		if ( file.includes(".js") === true ) {
+			console.log(`Js file = ${file}`);
+		}
+    });
+});
